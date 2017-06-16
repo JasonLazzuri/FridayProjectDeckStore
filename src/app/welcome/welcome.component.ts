6f1b733c7cd0e42c3132f01d61c68ef0fe316931
JasonLazzuri <jason.Lazzuri@gmail.com>
2017-06-16 16:18:01 -0700

    finish styling. add pictures and more text

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent implements OnInit {
-
+title = 'A Great Place To Store Your Decks';
   constructor() { }
 
   ngOnInit() {
